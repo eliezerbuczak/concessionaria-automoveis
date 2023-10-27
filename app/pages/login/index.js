203f3f224c92d6fcda6612de5bf641af0a4eaebd
eliezer <eliezerbuczak@gmail.com>
2023-10-26 22:00:51 -0300

    autenticacao

--- app/pages/login/index.js
+++ app/pages/login/index.js
@@ -1,6 +1,6 @@
-
-const baseURL = 'http://localhost:3000';
 document.addEventListener("DOMContentLoaded", function () {
+  const baseURL = 'http://localhost:3000';
+
   const usernameInput = document.getElementById("username");
   const passwordInput = document.getElementById("password");
   const loginButton = document.getElementById("login-button");
@@ -10,24 +10,29 @@ document.addEventListener("DOMContentLoaded", function () {
     const username = usernameInput.value;
     const password = passwordInput.value;
 
-    //make a request to the server to check if the user exists and the password is correct in table user com json server
-
-    fetch(`${baseURL}/users`).then((response) => {
-      if (!response.ok) {
-        throw new Error("A resposta não está OK");
-      }
-      return response.json();
-    }).then(data => {
-      data.forEach(user => {
-        if (user.user === username && user.password === password) {
-          localStorage.setItem("authenticated", true);
-          window.location.href = "/app/pages/automoveis/index.html";
-        }
-      })
-    }).catch((error) => {
-      console.error("Erro:", error);
-    })
+    authenticateUser(username, password);
   });
 
-
-});
+  const authenticateUser = async (username, password) => {
+    // Fazer uma solicitação ao servidor para verificar se o usuário existe e a senha está correta na tabela de usuários com JSON Server
+    await fetch(`${baseURL}/users`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("A resposta não está OK");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        data.forEach((user) => {
+          if (user.user === username && user.password === password) {
+            localStorage.setItem("authenticated", true);
+            window.location.href = "/app/pages/automoveis/index.html";
+          }
+        });
+      })
+      .catch((error) => {
+        console.error("Erro:", error);
+      })
+  }
+}
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     colors: {
-      'yellow' : '#ECC440',
-      'white' : '#FFFFFF',
-      'black' : '#000000',
-      'gray' : '#00000050'
+      'yellow': '#ECC440',
+      'white': '#FFFFFF',
+      'black': '#000000',
+      'gray': '#00000050'
     },
   },
   plugins: [],
